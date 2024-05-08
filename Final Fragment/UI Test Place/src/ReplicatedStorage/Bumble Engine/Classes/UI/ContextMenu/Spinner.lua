e8ca8794f79690834f899a50cc01efdab28ccfed
William Bange <wbange18@gmail.com>
2024-05-08 13:39:28 -0400

    Ring Spinner finished (without debug)

--- Final Fragment/UI Test Place/src/ReplicatedStorage/Bumble Engine/Classes/UI/ContextMenu/Spinner.lua
+++ Final Fragment/UI Test Place/src/ReplicatedStorage/Bumble Engine/Classes/UI/ContextMenu/Spinner.lua
@@ -1,3 +1,7 @@
+local RunService = game:GetService("RunService")
+
+--Dangerous line, make sure this is loaded on client only.
+local Player = game.Players.LocalPlayer
 
 --[[Spinner: Simple class which provides spin animation features for the ContextFrame class.]]
 
@@ -7,29 +11,21 @@ Spinner.__index = Spinner
 
 --METHODS
 
---[[Run
-Run the spinner
+--[[ChangeSpeed
+Change the spinner's speed
+@param {number} speed - Speed to add to the spinner.
+@param {number} smoothing - Time to smooth the speed over.
 ]]
-function Spinner:Run()
-
-   coroutine.resume(self.Spin)
-   
-   return
-end
-
---[[Yield
-Stop the spinner
-]]
-function Spinner:Yield()
-   coroutine.yield(self.Spin)
-   return
-end
-
---[[Change
-Change the spinner's properties
-]]
-function Spinner:Change()
-   
+function Spinner:Change(speed, smoothing)
+   self.extraSpeed = speed
+   if smoothing ~= nil and smoothing ~= 0 then
+      coroutine.wrap(function()
+         repeat
+            self.extraSpeed += speed/smoothing
+            task.wait(.1)
+         until self.extraSpeed >= speed
+      end)()
+   end
    return
 end
 
@@ -46,8 +42,32 @@ function Spinner.new()
    newSpinner.ring1 = script.Parent.InnerRing
    newSpinner.ring2 = script.Parent.OuterRing
    
-   newSpinner.Spin = coroutine.create(function()
+   --Initialize iterative values
+   local playerSpeed = nil
+   local speedAdditive = 0
+   local angleForwards = 0
+   local angleBackwards = 0
+   
+   --Connect to the step after simulation, least impactful
+   newSpinner.Connection = RunService.PostSimulation:Connect(function()
+      --Get the velocity of the player's rootpart
+      playerSpeed = Player.Character.HumanoidRootPart.assemblylinearvelocity.magnitude
       
+      --Create a proper factor for the ring speed. Can be adjusted with external values
+      --[[Current Logic:
+      Player speed is in studs/sec, so divide by 60 assuming 60FPS
+      Extra speed additive is zero for now
+      Manual additive is 1 for now
+      ]]
+      speedAdditive = 1 + (playerSpeed/60) + newSpinner.extraSpeed
+      
+      --Calculate the forward and backward angles using the modulus of the maximum angle
+      angleForwards = (angleForwards + speedAdditive) % 180
+      angleBackwards = (angleBackwards - speedAdditive) % -180
+      
+      --Assign the rotation values instantaneously
+      newSpinner.Ring1.Rotation = angleForwards
+      newSpinner.Ring2.Rotation = angleBackwards
    end)
    
    return newSpinner
